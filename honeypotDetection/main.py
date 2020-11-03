03cc3ece1bae17432155a7a140f0d74eef7c9b8a
Dor <dors9590@gmail.com>
2020-11-03 20:34:50 +0200

    upgraded to the new win32 api

--- honeypotDetection/main.py
+++ honeypotDetection/main.py
@@ -1,25 +1,30 @@
-import LogListener
+import LogListenerv2
 import threading
 import time
-import win32evtlog
+
+
 def event_log_pulling(l_type):
     print(l_type)
-    current = LogListener.LogListener(l_type['server'], l_type['logType'])
-    current.listen(mutex)
+    current = LogListenerv2.LogListenerv2(l_type['logType'])
+    current.listen()
     return current
 
 
-mutex = threading.Lock()
-threads = []
-log_types = [{'logType': "System", 'server': "localhost"}, {'logType': "Security", 'server': "localhost"}]
-for log in log_types:
-    t = threading.Thread(target=event_log_pulling, args=[log])
-    t.daemon = True
-    t.start()
-    threads.append(t)
+def main():
+    threads = []
+    log_types = [{'logType': "Security"}]
+    for log in log_types:
+        t = threading.Thread(target=event_log_pulling, args=[log])
+        t.daemon = True
+        t.start()
+        threads.append(t)
 
-try:
-    while True:
-        time.sleep(2)
-except KeyboardInterrupt:
-    print(f'[-] Quitting the program')
+    try:
+        while True:
+            time.sleep(2)
+    except KeyboardInterrupt:
+        print(f'[-] Quitting the program')
+
+
+if __name__ == '__main__':
+    main()
