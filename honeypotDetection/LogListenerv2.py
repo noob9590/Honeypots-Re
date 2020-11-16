aa3605ea0d3bcf47e3d8658c88e05f41c92c83b6
Dor <dors9590@gmail.com>
2020-11-16 15:30:32 +0200

    added alert message format

--- honeypotDetection/LogListenerv2.py
+++ honeypotDetection/LogListenerv2.py
@@ -1,6 +1,8 @@
 import win32evtlog, win32event, win32con
 import json
 import xmltodict
+import requests
+import socket
 
 
 class LogListenerv2:
@@ -16,8 +18,40 @@ class LogListenerv2:
                 return False
         return True
 
-    def __alert(self):
-        print("alert")
+    def get_ip(self, hostname):
+        ip_address = socket.gethostbyname(hostname)
+        return ip_address
+
+    def __alert(self, data, event_id):
+        username = ""
+        honeypot_name = ""
+        computer_name = data['Event']['System']['Computer']
+        ip_address = self.get_ip(computer_name)
+        system_time = data['Event']['System']['TimeCreated']['@SystemTime']
+        if 'EventData' in data['Event'] and 'SubjectUserName' in data['Event']['EventData']:
+            username = data['Event']['EventData']['SubjectUserName']
+        elif 'UserData' in data['Event']:
+            user_data_keys = data['Event']['UserData'].keys()
+            subject_user_name_key = ""
+            for key in user_data_keys:
+                if 'SubjectUserName' in data['Event']['UserData'][key]:
+                    subject_user_name_key = key
+                    break
+            username = data['Event']['UserData'][subject_user_name_key]['SubjectUserName']
+        else:
+            username = "No user information"
+        alert_format = {
+            "honeypot": honeypot_name,
+            "eventId": event_id,
+            "ipAddress": ip_address,
+            "computerName": computer_name,
+            "systemTime": system_time,
+            "username": username,
+            "data": json.dumps(data, sort_keys=False, indent=4)
+        }
+        print(json.dumps(alert_format, sort_keys=False, indent=4))
+        requests.post("http://localhost:3000", data=alert_format)
+
 
     def listen(self, honeypot_configuration):
         h = win32event.CreateEvent(None, 0, 0, None)
@@ -37,7 +71,7 @@ class LogListenerv2:
                     else:
                         event_id = event_format_dict['Event']['System']['EventID']['#text']
                     if self.__identify_honeypot(event_id, event_format_xml, honeypot_configuration) is True:
-                        self.__alert()
+                        self.__alert(event_format_dict, event_id)
             while True:
                 print("waiting...")
                 w = win32event.WaitForSingleObjectEx(h, 10000, True)
