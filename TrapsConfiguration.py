4b401f93e641063d6b87bd7ba1ef551c94b64c20
Dor <dors9590@gmail.com>
2020-11-16 15:25:49 +0200

    added alert format message

---
+++ TrapsConfiguration.py
@@ -0,0 +1,54 @@
+import sys
+import json
+import os
+
+
+class TrapsStructure:
+
+    def __init__(self):
+        self.configureList = [None] * 65535
+
+    def __export_to_json_file(self):
+        file_name = input("[+] Provide a name to the configuration file\n>>> ")
+        _dir = os.path.dirname(__file__)
+        file_path = os.path.join(_dir, file_name)
+        with open(file_path, 'w+') as f:
+            f.write(json.dumps(self.configureList))
+            print(f"[+]Configuration was saved to: {file_name}")
+
+    def configure(self):
+        print("[+] Begins a configuration process ")
+        index = 1
+        while True:
+            print("[+] Configuration: {}".format(index))
+            event_id = input("[+] Event ID\n>>> ")
+            try:
+                self.configureList[int(event_id)] = {
+                    "EventID": event_id,
+                    "EventData": []
+                }
+            except (ValueError, IndexError):
+                if event_id == "exit":
+                    self.__export_to_json_file()
+                else:
+                    print("[-] Invalid input\nQuitting the program")
+                    sys.exit(1)
+                return self.configureList
+            input_param = ">>> "
+            print("[+] EventData trap configuration")
+            while True:
+                event_data_param = input(input_param)
+                if event_data_param == "done":
+                    print(f"[+] The configuration for EventID: {event_id} was saved")
+                    break
+                self.configureList[int(event_id)]['EventData'].append(event_data_param)
+            index = index + 1
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,65 @@
+import LogListenerv2
+import threading
+import time
+import optparse
+import TrapsConfiguration
+import sys
+import os
+import json
+
+
+def event_log_pulling(l_type, json_obj):
+    current = LogListenerv2.LogListenerv2(l_type[0]['logType'])
+    current.listen(json_obj)
+    return current
+
+
+def arguments_parser():
+    parser = optparse.OptionParser()
+    parser.add_option("-f", "--file", dest="file_path", help="The file path for the configuration file")
+    parser.add_option("--start", dest="configuration", help="--start argument in order to begin configuration process")
+    (options, arguments) = parser.parse_args()
+    if options.file_path is None and options.configuration is None:
+        parser.print_help()
+        sys.exit(1)
+    return options
+
+
+def open_configuration_file(file_path):
+    try:
+        if os.path.isdir(file_path):
+            with open(os.path.dirname() + file_path, "r") as f:
+                return json.load(f)
+        else:
+            with open(file_path, "r") as f:
+                return json.load(f)
+    except FileNotFoundError:
+        print("No such file or directory")
+        sys.exit(1)
+
+
+def main():
+    json_file = None
+    threads = []
+    log_types = [{'logType': "System"}, {'logType': "Security"}, {'logType': 'Application'}, {'logType': "Setup"}]
+    option = arguments_parser()
+    if option.file_path is None:
+        conf = TrapsConfiguration.TrapsStructure()
+        honeypot_configuration = conf.configure()
+    else:
+        honeypot_configuration = open_configuration_file(option.file_path)
+    for log in log_types:
+        t = threading.Thread(target=event_log_pulling, args=([log], honeypot_configuration))
+        t.daemon = True
+        t.start()
+        threads.append(t)
+
+    try:
+        while True:
+            time.sleep(2)
+    except KeyboardInterrupt:
+        print(f'[-] Quitting the program')
+
+
+if __name__ == '__main__':
+    main()
