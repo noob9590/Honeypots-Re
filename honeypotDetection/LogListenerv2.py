efec5dc9d970c836e4f41239a593f5a8ec4c4405
Dor <dors9590@gmail.com>
2021-03-30 15:43:37 +0300

    fixing defects v4

--- honeypotDetection/LogListenerv2.py
+++ honeypotDetection/LogListenerv2.py
@@ -1,28 +1,67 @@
-import win32evtlog, win32event, win32con
 import json
-import xmltodict
-import requests
 import socket
-import lxml.etree as etree
+import requests
+import win32con
+import win32event
+import win32evtlog
+import xmltodict
+
+"""
+This program receives the honeypots configuration from TrapsConfiguration.py and searches for the corresponding data.
+
+Windows events have the following structure:
+<Event xlmns="...">
+    <System>
+        {some System information}
+        <IP>
+        <ComputerName>
+        ...
+    </System>
+    <EventData>
+        <Data Name = "ProcessId">{Process ID}</Data>
+        ...
+        ...
+    </EventData>
+</Event>
+
+if the data from the configuration file found in windows events:
+    the event is forwarding to the server in order to alert the system administrator from unauthorized entity
+
+in addition, the program categorize the event information for the convenience of the system administrator.
+
+"""
 
 
 class LogListenerv2:
+    # Constructor: initializes to search for any kind of event in the system for the specified log category.
     def __init__(self, log_type):
         self.query_text = '*'
         self.log_type = log_type
 
-    def __identify_honeypot(self, evt_id, evt_format, honeypot_conf):
-        if honeypot_conf[int(evt_id)] is None or honeypot_conf[int(evt_id)]['EventID'] != evt_id:
-            return False
-        for data in honeypot_conf[int(evt_id)]['EventData']:
+    # Method: helper method for __identify_honeypot  - checks for corresponding data from windows event
+    def __is_data_in_winevt(self, evt_data, evt_format):
+        for data in evt_data:
             if data not in evt_format:
                 return False
         return True
 
+    # Method: filtering the windows event by <event id> and then searches
+    # for corresponding information from 'honetpot_conf' file
+    # Returns the honeypot name when corresponding information is found
+    def __identify_honeypot(self, evt_id, evt_format, honeypot_conf):
+        if honeypot_conf.get(evt_id) is None:
+            return None
+        for events in honeypot_conf[evt_id]:
+            if self.__is_data_in_winevt(events['EventData'], evt_format) is True:
+                return events['honeypotName']
+        return None
+
+    # Method: resolves hostname to ip address
     def get_ip(self, hostname):
         ip_address = socket.gethostbyname(hostname)
         return ip_address
 
+    # Method: retrieves the username from the <EventData> section in windows event xml
     def __find_user_name_from_eventdata(self, data, event_info):
         target_user_name = None
         subject_user_name = None
@@ -39,6 +78,7 @@ class LogListenerv2:
             return "No user information"
         return "Subject: " + subject_user_name
 
+    # Method: retrieves the username from the <UserData> section in windows event xml
     def __find_user_from_userdata(self, data, event_info):
         event_info_keys = data[event_info].keys()
         for key in event_info_keys:
@@ -48,9 +88,10 @@ class LogListenerv2:
                 continue
         return "No User Information"
 
-
+    # Method: identifying if windows event has category of <UserData> or <EventData>
     def __event_category(self, data):
-        if self.log_type == 'Security' or ('RenderingInfo' in data['Event'] and data['Event']['RenderingInfo']['Channel'] == 'Security'):
+        if self.log_type == 'Security' or (
+                'RenderingInfo' in data['Event'] and data['Event']['RenderingInfo']['Channel'] == 'Security'):
             print("Security")
             if "EventData" in data['Event']:
                 username = self.__find_user_name_from_eventdata(data['Event']['EventData'], 'Data')
@@ -62,6 +103,7 @@ class LogListenerv2:
             username = "No User Information"
         return username
 
+    # Method: sending an alert for the server after data filtration
     def __alert(self, data, event_id, honeypot_n):
         honeypot_name = honeypot_n
         computer_name = data['Event']['System']['Computer']
@@ -77,12 +119,17 @@ class LogListenerv2:
             "username": username,
             "data": json.dumps(data, sort_keys=False, indent=4)
         }
-        # print(json.dumps(alert_format, sort_keys=False, indent=4))
         requests.post("https://localhost:3002", data=alert_format, verify=False)
 
+    # This method is managing the class and act like the "main of the class".
+    # Method: listens for windows events
+    #         categorizes the data in windows event and filters the require data
+    #         checks for corresponding event information in order to alert the system administrator
+    #         sends the alerts to the server
     def listen(self, honeypot_configuration):
         h = win32event.CreateEvent(None, 0, 0, None)
-        s = win32evtlog.EvtSubscribe(self.log_type, win32evtlog.EvtSubscribeStartAtOldestRecord, SignalEvent=h, Query=self.query_text)
+        s = win32evtlog.EvtSubscribe(self.log_type, win32evtlog.EvtSubscribeStartAtOldestRecord, SignalEvent=h,
+                                     Query=self.query_text)
         while True:
             while True:
                 events = win32evtlog.EvtNext(s, 10)
@@ -96,10 +143,11 @@ class LogListenerv2:
                         event_id = event_format_dict['Event']['System']['EventID']
                     else:
                         event_id = event_format_dict['Event']['System']['EventID']['#text']
-                    if self.__identify_honeypot(event_id, event_format_xml, honeypot_configuration):
-                        self.__alert(event_format_dict, event_id, honeypot_configuration[int(event_id)]['honeypotName'])
+                    honeypot = self.__identify_honeypot(event_id, event_format_xml, honeypot_configuration)
+                    if honeypot is not None:
+                        self.__alert(event_format_dict, event_id, honeypot)
             while True:
-                print("waiting...")
+                print("Waiting " + self.log_type)
                 w = win32event.WaitForSingleObjectEx(h, 10000, True)
                 if w == win32con.WAIT_OBJECT_0:
                     break
--- honeypotDetection/main.py
+++ honeypotDetection/main.py
@@ -6,14 +6,34 @@ import TrapsConfiguration
 import sys
 import os
 import json
+"""
+Windows events divided to several categories:
+    
+    Application    Security    System    Setup    Forwarded Events
+    
+In order to listen on each category there is a need of the use multi threading.
+Each thread listen for a different category and one more thread for the main. 
+
+The program manage the whole agent by the following order:
+
+1) either starts the process the honeypot configuration and saves it into a *.txt file
+    if the file already exists the program skip on the honeypot configuration
+    
+2) creates threads for the number of the event categories and uses LogListenerV2.py in order the identify an honeypot
+
+3) endless loop to repeat the process
+
+"""
 
 
+# Method: event handler for threads
 def event_log_pulling(l_type, json_obj):
     current = LogListenerv2.LogListenerv2(l_type[0]['logType'])
     current.listen(json_obj)
     return current
 
 
+# Method: creates help menu for the agent
 def arguments_parser():
     parser = optparse.OptionParser()
     parser.add_option("-f", "--file", dest="file_path", help="The file path for the configuration file")
@@ -25,6 +45,7 @@ def arguments_parser():
     return options
 
 
+# Method: opens the configuration file from TrapsConfiguration.py
 def open_configuration_file(file_path):
     try:
         if os.path.isdir(file_path):
@@ -38,6 +59,7 @@ def open_configuration_file(file_path):
         sys.exit(1)
 
 
+# main
 def main():
     json_file = None
     threads = []
